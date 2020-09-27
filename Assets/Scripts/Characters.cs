91d3eb0890342e3c5564af7a3e5c97258330cd3f
Theo O <togomiti@hotmail.fr>
2020-09-27 21:30:47 +0200

    bnhcuidshuofchzuofa

--- Assets/Scripts/Characters.cs
+++ Assets/Scripts/Characters.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Characters : MonoBehaviour
 {
@@ -21,12 +23,54 @@ public class Characters : MonoBehaviour
 
     [SerializeField] private float talkTime;
     [SerializeField] private float breakTime;
+
+    [SerializeField] private GameObject dialogue1;
+    [SerializeField] private GameObject dialogue2;
+    [SerializeField] private Text dialogueText1;
+    [SerializeField] private Text dialogueText2;
+
+    private List<string> _redDialogues;
+    private List<string> _yellowDialogues;
+    private List<string> _greenDialogues;
     
     private static readonly int Stop = Animator.StringToHash("Stop");
     private static readonly int StartTalking = Animator.StringToHash("StartTalking");
     private static readonly int StartRolling = Animator.StringToHash("StartRolling");
     private static readonly int StartYawning = Animator.StringToHash("StartYawning");
     private static readonly int StartDrinking = Animator.StringToHash("StartDrinking");
+
+    private void Awake()
+    {
+        _redDialogues = new List<string>
+        {
+            "Bloody hell, what is wrong with you? ",
+            "Listen, I'm not so pumped up tonight",
+            "I think this whole date might be in vein",
+            "Sorry but I am more of a AB-type person",
+            "Maybe we should start again from scratch?",
+            "This place/this date sucks"
+        };
+
+        _yellowDialogues = new List<string>
+        {
+            "As a software developer I do get rid of bugs",
+            "I only bite vegetarians",
+            "Have you found still waters to stay nearby?",
+            "Ever been to the mosque-ito?",
+            "Do you often go skin-diving? ",
+            "I mostly listen to Sting",
+            "Are you into astronomy? Because tonight's the blood moon"
+        };
+
+        _greenDialogues = new List<string>
+        {
+            "You're bloody gorgeous, you know that?",
+            "One would say this date is a total success",
+            "I'm actually itching for another drink",
+            "A human tried to clap me once",
+            "I got a déjà vu, aren't we blood related?"
+        };
+    }
 
     private void Start()
     {
@@ -53,10 +97,14 @@ public class Characters : MonoBehaviour
             c1.SetTrigger(StartTalking);
             var ratio = (gameManager.DrinkGauge + gameManager.TalkGauge) / 200f;
             c2.SetTrigger(ratio < 0.25 ? StartRolling : ratio < 0.5 ? StartYawning : StartDrinking);
+            (c1 == white ? dialogue1 : dialogue2).SetActive(true);
+            var dialogues = (ratio < 0.25 ? _redDialogues : ratio < 0.5 ? _yellowDialogues : _greenDialogues);
+            (c1 == white ? dialogueText1 : dialogueText2).text = dialogues[Random.Range(0, dialogues.Count)];
             yield return new WaitForSeconds(talkTime);
             
             c1.SetTrigger(Stop);
             c2.SetTrigger(Stop);
+            (c1 == white ? dialogue1 : dialogue2).SetActive(false);
 
             var cTemp = c2;
             c2 = c1;
