b66d49522dfb6f377bfe9231bcb1201da258b627
Damien Drouillard <damien.drouillard.auditeur@lecnam.net>
2020-09-27 20:52:40 +0200

    🚧Fix Ducking for sounds

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -52,7 +52,6 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID ATMOS = 3034623661U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID VOICES = 3313685232U;
